f1e00882f52beb471647c08b929e96b1b17e59a5
jzhou1-02 <zhoujustin02@gmail.com>
2022-06-03 09:54:42 -0400

    sdfas

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,6 +1,6 @@
 import "../styles/style.css";
 
-window.onbeforeunload = function () {
+/* window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
 
@@ -9,4 +9,15 @@ document.getElementById("button2").addEventListener("click", function () {
 });
 document.getElementById("button3").addEventListener("click", function () {
   gsap.to(window, { duration: 1, scrollTo: "#food" });
+});
+ */
+
+gsap.from("#arch", {
+  x: 2000,
+  scrollTrigger: "#arch",
+});
+
+gsap.from("#food", {
+  x: 2000,
+  scrollTrigger: "#food",
 });
