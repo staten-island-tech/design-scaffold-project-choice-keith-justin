d49254d652370a53f2bdef92eb555561ea434537
Keith Shao <KeithS7@nycstudents.net>
2022-06-03 09:40:16 -0400

    blog

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,1 +1,12 @@
 import "../styles/style.css";
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
+document.getElementById("button2").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#arch" });
+});
+document.getElementById("button3").addEventListener("click", function () {
+  gsap.to(window, { duration: 1, scrollTo: "#food" });
+});
