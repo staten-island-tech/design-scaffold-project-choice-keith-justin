055bbd71484bee6f8f6ca62482db60ae633a66ed
Keith Shao <KeithS7@nycstudents.net>
2022-06-06 10:14:02 -0400

    blog

--- blog/js/main.js
+++ blog/js/main.js
@@ -1,23 +1,27 @@
 import "../styles/style.css";
 
-/* window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-};
+// window.onbeforeunload = function () {
+//   window.scrollTo(0, 0);
+// };
 
-document.getElementById("button2").addEventListener("click", function () {
-  gsap.to(window, { duration: 1, scrollTo: "#arch" });
-});
-document.getElementById("button3").addEventListener("click", function () {
-  gsap.to(window, { duration: 1, scrollTo: "#food" });
-});
- */
+// document.getElementById("button2").addEventListener("click", function () {
+//   gsap.to(window, { duration: 1, scrollTo: "#arch" });
+// });
+// document.getElementById("button3").addEventListener("click", function () {
+//   gsap.to(window, { duration: 1, scrollTo: "#food" });
+// });
 
-gsap.from("#arch", {
+gsap.from(".top", {
+  duration: 1,
+  y: "-100%",
   x: 2000,
+  ease: "bounce",
   scrollTrigger: "#arch",
 });
 
-gsap.from("#food", {
+gsap.from("#featured", {
+  duration: 1,
+  delay: 1,
   x: 2000,
   scrollTrigger: "#food",
 });
